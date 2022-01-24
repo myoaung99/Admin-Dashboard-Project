16bcb41ae7813fd87a2472b0fe8561cf868769c2
MyoMyintAung <myomyintaung064@gmail.com>
2022-01-24 13:40:48 +0630

    Add pie chart and make responsive

--- js/dashboard.js
+++ js/dashboard.js
@@ -68,3 +68,66 @@ const myChart = new Chart(ctx, {
     },
   },
 });
+
+let orderFromPlace = [5, 15, 4, 9, 7];
+let places = ["YGN", "MDY", "NPY", "SHAN", "MGW"];
+
+const pv = document.getElementById("pv").getContext("2d");
+const pvChart = new Chart(pv, {
+  type: "doughnut",
+  data: {
+    labels: places,
+    datasets: [
+      {
+        label: "# of Votes",
+        data: orderFromPlace,
+        backgroundColor: [
+          "rgba(255, 99, 132, 0.2)",
+          "rgba(54, 162, 235, 0.2)",
+          "rgba(255, 206, 86, 0.2)",
+          "rgba(75, 192, 192, 0.2)",
+          "rgba(153, 102, 255, 0.2)",
+          "rgba(255, 159, 64, 0.2)",
+        ],
+        borderColor: [
+          "rgba(255, 99, 132, 1)",
+          "rgba(54, 162, 235, 1)",
+          "rgba(255, 206, 86, 1)",
+          "rgba(75, 192, 192, 1)",
+          "rgba(153, 102, 255, 1)",
+          "rgba(255, 159, 64, 1)",
+        ],
+        borderWidth: 1,
+      },
+    ],
+  },
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+        grid: {
+          display: false,
+        },
+        ticks: {
+          display: false,
+        },
+      },
+      x: {
+        grid: {
+          display: false,
+        },
+        ticks: {
+          display: false,
+        },
+      },
+    },
+    plugins: {
+      legend: {
+        position: "bottom",
+        labels: {
+          usePointStyle: true,
+        },
+      },
+    },
+  },
+});
